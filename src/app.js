5b989502e0da1547fd93d19960e65c5a1810fd74
GulnurYuksel <gulnuryuksel96@gmail.com>
2021-10-19 00:09:07 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhaba Kodlama.io")
+
+/*let dolarDun = 9.20
+let dolarBugun = 9.30
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)*/
+
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+ konutKredileri[index] +"</li")
+    
+}
+console.log("</ul>")
+console.log(konutKredileri)
